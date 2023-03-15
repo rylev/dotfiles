025b56390ab13685f38f110785c86990ede262a6
Ryan Levick <me@ryanlevick.com>
2023-03-15 20:08:43 +0100

    Add some basic gnome configuration

--- README.md
+++ README.md
@@ -23,8 +23,6 @@ ansible-playbook desktop.yml --ask-become-pass
 After running ansible there are still some things that need manual configuration:
 
 * Ensuring that the date/time format is correct (usually set to U.S. instead of international)
-* Remapping caps-lock to control
-  * Done using gnome-tweaks
 * Logging into various services:
   * 1Password
   * Googgle
--- install-ansible.sh
+++ install-ansible.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install python3 python3-pip -y
+
 if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     echo "Adding Ansible PPA"
     sudo apt-add-repository ppa:ansible/ansible -y
@@ -7,7 +10,6 @@ fi
 
 if ! hash ansible >/dev/null 2>&1; then
     echo "Installing Ansible..."
-    sudo apt-get update
     sudo apt-get install ansible git ansible-playbook -y
 else
     echo "Ansible already installed"
